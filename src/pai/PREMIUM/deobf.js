02a2410f03484f90f75f30203b3b9a20a876e3c0
Mahdiisdumb <mahdihamade6789010@outlook.com>
2025-12-09 21:17:54 -0500

    x

--- src/pai/PREMIUM/deobf.js
+++ src/pai/PREMIUM/deobf.js
@@ -1,4 +1,4 @@
-const __OBFUSCATED_CODE='PAPD9792';window.Deobf=(function(){function _shiftChar(ch,shift){const c=ch.charCodeAt(0);if(c>=65&&c<=90){const base=65;return String.fromCharCode(((c-base+shift)%26+26)%26+base)}
+const __OBFUSCATED_CODE='PAPDMOBJ';window.Deobf=(function(){function _shiftChar(ch,shift){const c=ch.charCodeAt(0);if(c>=65&&c<=90){const base=65;return String.fromCharCode(((c-base+shift)%26+26)%26+base)}
 if(c>=97&&c<=122){const base=97;return String.fromCharCode(((c-base+shift)%26+26)%26+base)}
 if(c>=48&&c<=57){const base=48;return String.fromCharCode(((c-base+shift)%10+10)%10+base)}
 return ch}
