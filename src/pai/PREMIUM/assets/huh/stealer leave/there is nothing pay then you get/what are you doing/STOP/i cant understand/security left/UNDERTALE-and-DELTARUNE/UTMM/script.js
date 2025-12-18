7ac942b82158c9d5745b5b77b44d98f196ef9bfb
Mahdiisdumb <mahdihamade6789010@outlook.com>
2025-12-17 19:59:37 -0500

    dsa

---
+++ src/pai/PREMIUM/assets/huh/stealer leave/there is nothing pay then you get/what are you doing/STOP/i cant understand/security left/UNDERTALE-and-DELTARUNE/UTMM/script.js
@@ -0,0 +1,3 @@
+document.getElementById("h").onclick = function () {
+    location.href = "https://sdsg-ut-mahdiidumbs-music-mods.vercel.app/"; // Redirects to the parent directory and then to load.html
+}
