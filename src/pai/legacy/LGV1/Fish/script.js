e4796b959a16968d20f4b48a39855d6b03c605dd
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-01-20 17:54:53 -0500

    Minified LGV1

--- src/pai/legacy/LGV1/Fish/script.js
+++ src/pai/legacy/LGV1/Fish/script.js
@@ -1,11 +1,1 @@
-document.getElementById('changeButton').addEventListener('click', function() {
-    const fishGif = document.getElementById('fishGif');
-    const originalSrc = 'https://media1.tenor.com/m/R5IECfIf34YAAAAd/fish-spinning.gif';
-    const newSrc = 'https://i.ytimg.com/vi/fUAyferAEpA/maxresdefault.jpg'; // New funny fish GIF
-
-    fishGif.src = newSrc;
-
-    setTimeout(() => {
-        fishGif.src = originalSrc;
-    }, 5000);
-});
+document.getElementById('changeButton').addEventListener('click',function(){const fishGif=document.getElementById('fishGif');const originalSrc='https://media1.tenor.com/m/R5IECfIf34YAAAAd/fish-spinning.gif';const newSrc='https://i.ytimg.com/vi/fUAyferAEpA/maxresdefault.jpg';fishGif.src=newSrc;setTimeout(()=>{fishGif.src=originalSrc},5000)})
--- src/pai/legacy/LGV1/MOTW/script.js
+++ src/pai/legacy/LGV1/MOTW/script.js
@@ -1,3 +1,1 @@
-document.getElementById('messageButton').addEventListener('click', function() {
-    document.getElementById('messageDisplay').innerText = 'One day you will be coverd up in sucsess.'; // every week change this
-});
+document.getElementById('messageButton').addEventListener('click',function(){document.getElementById('messageDisplay').innerText='One day you will be coverd up in sucsess.'})
