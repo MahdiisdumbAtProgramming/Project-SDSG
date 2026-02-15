d1b386da1f8168a78ec2792689a54822c623b45b
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-02-15 12:11:13 -0500

    ds

---
+++ build.bat
@@ -0,0 +1,40 @@
+@echo off
+:: Set console color to light green
+color a
+
+:: Print the title
+echo [--------------------------]
+echo [ BUILDING PROJECT SDSG----]
+echo [-----BY: MAHDIISDUMB------]
+echo [--------------------------]
+
+:: Run the Node build
+echo Running Node build...
+node REL
+if errorlevel 1 (
+    echo Node build failed!
+    pause
+    exit /b 1
+)
+
+:: Compress output with 7-Zip
+:: Make sure 7z.exe is in PATH or adjust the path below
+set OUTPUT_DIR=dist
+set ZIP_NAME=Project_SDSG.7z
+
+if not exist "%OUTPUT_DIR%" (
+    echo Output directory "%OUTPUT_DIR%" not found!
+    pause
+    exit /b 1
+)
+
+echo Compressing build into %ZIP_NAME%...
+7z a -t7z "%ZIP_NAME%" "%OUTPUT_DIR%\*"
+if errorlevel 1 (
+    echo Compression failed!
+    pause
+    exit /b 1
+)
+
+echo Build complete!
+pause
