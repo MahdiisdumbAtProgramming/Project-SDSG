d7231131a70d0e2bab6eddd56b2921926a3bb1e1
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-02-22 16:46:47 -0500

    dsa

--- build.bat
+++ build.bat
@@ -1,13 +1,9 @@
 @echo off
 color a
-
 echo [--------------------------]
 echo [ BUILDING PROJECT SDSG----]
 echo [-----BY: MAHDIISDUMB------]
 echo [--------------------------]
-echo.
-
-:: Run the Node build
 echo Running Node build...
 node REL
 if errorlevel 1 (
@@ -15,33 +11,16 @@ if errorlevel 1 (
     pause
     exit /b 1
 )
-
-:: Create a zip of the current folder
 set ZIP_NAME=Project_SDSG.zip
-if exist "%ZIP_NAME%" del "%ZIP_NAME%"
-powershell -command "Add-Type -AssemblyName 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('.', '%ZIP_NAME%')"
-
-:: Split the zip into 50 MB chunks
-set PART_SIZE=52428800
-set /a INDEX=1
-
-echo Splitting zip into 50 MB parts...
-
-:split_loop
 if not exist "%ZIP_NAME%" (
-    echo Zip file not found!
+    echo Zip file not found! REL should have made it.
     pause
     exit /b 1
 )
-
-fsutil file createnew temp.bin %PART_SIZE% 2>nul
-for /f "skip=0 delims=" %%A in ('certutil -encodehex "%ZIP_NAME%" temp.txt 0x40000000 ^| find /v ":"') do (
-    rem nothing here, certutil will generate temp.txt
-)
-
-:: simpler approach: use powershell for proper splitting
+set PART_SIZE=103809024
+set /a INDEX=0
+echo Splitting "%ZIP_NAME%" into 99 MB parts...
 powershell -command ^
-"$infile='%ZIP_NAME%'; $size=%PART_SIZE%; $i=0; [IO.File]::OpenRead($infile) | % { $buf = New-Object byte[] $size; while ($r = $_.Read($buf,0,$size)) { Set-Content -Encoding Byte ('%ZIP_NAME%.part'+$i) $buf[0..($r-1)]; $i++ } }"
-
+"$infile='%ZIP_NAME%'; $size=%PART_SIZE%; $i=0; $fs = [IO.File]::OpenRead($infile); try { while ($fs.Position -lt $fs.Length) { $buf = New-Object byte[] ([Math]::Min($size, $fs.Length - $fs.Position)); $fs.Read($buf, 0, $buf.Length) | Out-Null; $partName = ('%ZIP_NAME%.part{0:000}' -f $i); [IO.File]::WriteAllBytes($partName, $buf); $i++ } } finally { $fs.Close() }"
 echo Split complete!
 pause
